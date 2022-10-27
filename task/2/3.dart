a73f3a7e15ce243ce3a1986ea045beb7b7b2f452
Ali Ammar <aliammarali125@gmail.com>
2022-10-27 17:52:04 +0300

    add listen two add task two

---
+++ task/2/3.dart
@@ -0,0 +1,18 @@
+/*
+write a program to enter 3 number  and then print the following case
+number 1 is bigger than 2 : false
+number 2 is bigger than 3 : true
+number 3 is equal number 1 : false
+
+
+
+the fianl output should be like this :
+
+1
+5
+3
+number 1 is bigger than 2 : false
+number 2 is bigger than 3 : true
+number 3 is equal number 1 : false
+
+ */
